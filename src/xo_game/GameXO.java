de8cec8b098fdfb1e3b18e405aeab1c33e289db8
Alex <aleksey241980@mail.ru>
2017-03-06 18:08:22 +0300

    Игра крестики нолики создан класс.

---
+++ src/xo_game/GameXO.java
@@ -0,0 +1,14 @@
+package xo_game;
+
+/**
+ * Java. Console game "Cross-zero"
+ * @author Aleksey Malyshev
+ * @version 1.0.0 dated March 06, 2017
+ */
+
+public class GameXO {
+    public static void main (String[] args){
+
+
+    }
+}
